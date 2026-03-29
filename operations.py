8fdd1c2e091ab9c236d4f3aae88a542b4d034c64
Praneeth <praneeth2621@gmail.com>
2026-03-29 18:30:30 +0000

    modified-user choosen

--- operations.py
+++ operations.py
@@ -4,3 +4,19 @@ print(f"addition of {a} and {b} is {a+b}")
 
 def multiplication(a,b):
 	print(f"product of {a} and {b} is {a+b}")
+
+print("1.add\n2.subtract\n3.multiply\n4.division\n5.exit\n")
+choice=int(input("Enter  choice :"))
+while(True):
+	if choice==1:
+		addition(a,b)
+	elif choice==2:
+		subtraction(a,b)
+	elif choice==3:
+		multiplication(a,b)
+	elif  choice==4:
+		division(a,b)
+	elif  choice==5:
+		exit
+	else:
+		print("Invalid Entry")
